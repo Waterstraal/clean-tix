79427f1593a98105c6fb8751433364d9db9a8332
Mike Markus <mike.markus@ortec-finance.com>
2022-05-04 14:14:40 +0200

    Small jquery tweaks

--- tix-bookmarklet.js
+++ tix-bookmarklet.js
@@ -1,21 +1,24 @@
 const bookmarkletCode = () => {
-    const cleaned = $('body').data('cleaned');
+    const body$ = $('body');
+    const cleaned = body$.data('cleaned');
     if (!cleaned) {
-        $('.type-of-work-title:contains(Direct hours)').each((v, el) => {
+        const matrix$ = $('.matrix-component');
+
+        matrix$.find('.type-of-work-title:contains(Direct hours)').each((v, el) => {
             const $el = $(el);
             const $topLevel = $el.parent().parent().prev();
             const startIndexName = $topLevel.text().search(/[a-zA-Z]/);
-            const dsc = $topLevel.text().substr(startIndexName);
+            const dsc = $topLevel.text().substring(startIndexName);
             $el.find('.title-part-item').append(': ' + dsc);
             $topLevel.hide();
         });
 
-        $('.matrix-component').on('focus', '.input', (el) => {
+        matrix$.on('focus', '.input', (el) => {
             const index = $(el.currentTarget).parents('.matrix-cell-component').index() + 1;
             $('.matrix-cell-component:nth-child(' + index + ')').addClass('is-focussed')
         });
 
-        $('.matrix-component').on('blur', '.input', (el) => {
+        matrix$.on('blur', '.input', () => {
             $('.matrix-cell-component').removeClass('is-focussed');
         });
 
@@ -29,6 +32,6 @@ const bookmarkletCode = () => {
 
         $('<style>').text(styles).appendTo(document.head);
 
-        $('body').data('cleaned', true);
+        body$.data('cleaned', true);
     }
 };
